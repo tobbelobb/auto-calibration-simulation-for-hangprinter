c761186a07186a1a58e86e806954a4259023867d
tobbelobb <tobben@fastmail.fm>
2019-01-13 18:18:48 +0100

    WIP: Structure of motor pos samp solver ready.

--- simulation.py
+++ simulation.py
@@ -16,6 +16,7 @@ X = 0
 Y = 1
 Z = 2
 params_anch = 9
+params_buildup = 5 # four spool radii, one spool buildup factor
 A_bx = 2
 A_cx = 5
 
@@ -191,9 +192,9 @@ def motor_pos_samples_to_line_length_with_buildup_compensation(
     motor_samps,
     spool_buildup_factor=0.008,  # Qualified first guess for 0.5 mm line
     spool_r=np.array([65.0, 65.0, 65.0, 65.0]),
-    spool_to_motor_gearing_factor=12.75,
-    mech_adv=np.array([2.0, 2.0, 2.0, 2.0]),
-    number_of_lines_per_spool=np.array([1.0, 1.0, 1.0, 1.0]),
+    spool_to_motor_gearing_factor=12.75,  # HP4 default (255/20)
+    mech_adv=np.array([2.0, 2.0, 2.0, 2.0]),  # HP4 default
+    number_of_lines_per_spool=np.array([1.0, 1.0, 1.0, 1.0]),  # HP4 default
 ):
     # Buildup per line times lines. Minus sign because more line in air means less line on spool
     c1 = -mech_adv * number_of_lines_per_spool * spool_buildup_factor
@@ -247,6 +248,27 @@ def cost_sq(anchors, pos, samp):
     )  # Sum of squares
 
 
+def cost_sq_for_pos_samp(anchors, pos, motor_pos_samp, spool_buildup_factor, spool_r):
+    """
+    For all samples sum
+    (Sample value if anchor position A and cartesian position x were guessed   - actual sample)^2
+
+    (sqrt((A_ax-x_i)^2 + (A_ay-y_i)^2 + (A_az-z_i)^2) - sqrt(A_ax^2 + A_ay^2 + A_az^2) - motor_pos_to_samp(t_ia))^2 +
+    (sqrt((A_bx-x_i)^2 + (A_by-y_i)^2 + (A_bz-z_i)^2) - sqrt(A_bx^2 + A_by^2 + A_bz^2) - motor_pos_to_samp(t_ib))^2 +
+    (sqrt((A_cx-x_i)^2 + (A_cy-y_i)^2 + (A_cz-z_i)^2) - sqrt(A_cx^2 + A_cy^2 + A_cz^2) - motor_pos_to_samp(t_ic))^2 +
+    (sqrt((A_dx-x_i)^2 + (A_dy-y_i)^2 + (A_dz-z_i)^2) - sqrt(A_dx^2 + A_dy^2 + A_dz^2) - motor_pos_to_samp(t_id))^2
+    """
+    return np.sum(
+        pow(
+            samples_relative_to_origin_no_fuzz(anchors, pos)
+            - motor_pos_samples_to_line_length_with_buildup_compensation(
+                motor_pos_samp, spool_buildup_factor, spool_r
+            ),
+            2,
+        )
+    )  # Sum of squares
+
+
 def anchorsvec2matrix(anchorsvec):
     """ Create a 4x3 anchors matrix from 6 element anchors vector.
     """
@@ -285,15 +307,15 @@ def posmatrix2vec(m):
     return np.reshape(m, np.shape(m)[0] * 3)
 
 
-def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
+def solve(motor_pos_samp, xyz_of_samp, _cost, method, cx_is_positive=False):
     """Find reasonable positions and anchors given a set of samples.
     """
 
-    u = np.shape(samp)[0]
+    u = np.shape(motor_pos_samp)[0]
     ux = np.shape(xyz_of_samp)[0]
     number_of_params_pos = 3 * (u - ux)
 
-    def costx(posvec, anchvec):
+    def costx(posvec, anchvec, spool_buildup_factor, spool_r):
         """Identical to cost, except the shape of inputs and capture of samp, xyz_of_samp, ux, and u
 
         Parameters
@@ -306,7 +328,22 @@ def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
         if np.size(xyz_of_samp) != 0:
             pos[0:ux] = xyz_of_samp
         pos[ux:] = np.reshape(posvec, (u - ux, 3))
-        return _cost(anchors, pos, samp)
+        return _cost(anchors, pos, motor_pos_samp, spool_buildup_factor, spool_r)
+
+    def costx_constant_buildup_params(posvec, anchvec):
+        """Identical to cost, except the shape of inputs and capture of samp, xyz_of_samp, ux, and u
+
+        Parameters
+        ----------
+        x : [A_ay A_az A_bx A_by A_bz A_cx A_cy A_cz A_dz
+               x1   y1   z1   x2   y2   z2   ...  xu   yu   zu
+        """
+        anchors = anchorsvec2matrix(anchvec)
+        pos = np.zeros((u, 3))
+        if np.size(xyz_of_samp) != 0:
+            pos[0:ux] = xyz_of_samp
+        pos[ux:] = np.reshape(posvec, (u - ux, 3))
+        return _cost(anchors, pos, motor_pos_samp, 0.008, np.array([65, 65, 65, 65]))
 
     l_long = 5000.0
     l_short = 1700.0
@@ -332,7 +369,7 @@ def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
         0.0,  # A_cy > 0
         -l_short,  # A_cz > -1700.0
         0.0,  # A_dz > 0
-    ] + [-l_short, -l_short, data_z_min] * (u - ux)
+    ] + [-l_short, -l_short, data_z_min] * (u - ux) + [0.0001, 1, 1, 1, 1]
     ub = [
         0.0,  # A_ay < 0
         l_short,  # A_az < 1700
@@ -343,7 +380,7 @@ def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
         l_long,  # A_cy < 4000.0
         l_short,  # A_cz < 1700
         l_long,  # A_dz < 4000.0
-    ] + [l_short, l_short, 2 * l_short] * (u - ux)
+    ] + [l_short, l_short, 2 * l_short] * (u - ux) + [0.01, 1000, 1000, 1000, 1000]
 
     # If the user has input xyz data, then signs should be ok anyways
     if ux > 2:
@@ -363,7 +400,7 @@ def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
     anchors_est = np.array(
         [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
     )
-    x_guess0 = list(anchorsmatrix2vec(anchors_est)) + list(posmatrix2vec(pos_est0))
+    x_guess0 = list(anchorsmatrix2vec(anchors_est)) + list(posmatrix2vec(pos_est0)) + [0.001, 1, 1, 1, 1]
 
     if method == "PowellDirectionalSolver":
         from mystic.termination import (
@@ -377,26 +414,45 @@ def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
 
         target = 1.0
         term = Or((COG(generations=100), CollapseAt(target, generations=100)))
-        # Solver 0
+
+        # Solver with constant buildup compensation parameters
         solver0 = PowellDirectionalSolver(number_of_params_pos + params_anch)
         solver0.SetEvaluationLimits(evaluations=3200000, generations=10000)
         solver0.SetTermination(term)
-        solver0.SetInitialPoints(x_guess0)
-        solver0.SetStrictRanges(lb, ub)
-        solver0.Solve(lambda x: costx(x[params_anch:], x[0:params_anch]))
-        x_guess0 = solver0.bestSolution
+        solver0.SetInitialPoints(x_guess0[0:-params_buildup])
+        solver0.SetStrictRanges(lb[0:-params_buildup], ub[0:-params_buildup])
+        solver0.Solve(lambda x: costx_constant_buildup_params(x[params_anch:], x[0:params_anch]))
+        x_guess0[0:-params_buildup] = solver0.bestSolution
         # PowellDirectional sometimes finds new ways if kickstarted anew
         for i in range(1, 20):
             solver0 = PowellDirectionalSolver(number_of_params_pos + params_anch)
-            solver0.SetInitialPoints(x_guess0)
-            solver0.SetStrictRanges(lb, ub)
-            solver0.Solve(lambda x: costx(x[params_anch:], x[0:params_anch]))
-            x_guess0 = solver0.bestSolution
-        return x_guess0
+            solver0.SetInitialPoints(x_guess0[0:-params_buildup])
+            solver0.SetStrictRanges(lb[0:-params_buildup], ub[0:-params_buildup])
+            solver0.Solve(lambda x: costx_constant_buildup_params(x[params_anch:], x[0:params_anch]))
+            x_guess0[0:-params_buildup] = solver0.bestSolution
+
+        # Solver 0
+        #solver0 = PowellDirectionalSolver(number_of_params_pos + params_anch + params_buildup)
+        #solver0.SetEvaluationLimits(evaluations=3200000, generations=10000)
+        #solver0.SetTermination(term)
+        #solver0.SetInitialPoints(x_guess0)
+        #solver0.SetStrictRanges(lb, ub)
+        #solver0.Solve(lambda x: costx(x[params_anch:-params_buildup], x[0:params_anch], x[-params_buildup], x[-params_buildup+1:]))
+        #x_guess0 = solver0.bestSolution
+        ## PowellDirectional sometimes finds new ways if kickstarted anew
+        #for i in range(1, 20):
+        #    solver0 = PowellDirectionalSolver(number_of_params_pos + params_anch + params_buildup)
+        #    solver0.SetInitialPoints(x_guess0)
+        #    solver0.SetStrictRanges(lb, ub)
+        #    solver0.Solve(lambda x: costx(x[params_anch:-params_buildup], x[0:params_anch], x[-params_buildup], x[-params_buildup+1:]))
+        #    x_guess0 = solver0.bestSolution
+        #return x_guess0
+
+
     elif method == "SLSQP":
-        # 'SLSQP' is crazy fast and lands on 0.0000 error
+        # 'SLSQP' is crazy fast but requires good data
         x_guess0 = scipy.optimize.minimize(
-            lambda x: costx(x[params_anch:], x[0:params_anch]),
+            lambda x: costx(x[params_anch:-params_buildup], x[0:params_anch], x[-params_buildup], x[-params_buildup+1:]),
             x_guess0,
             method=method,
             bounds=list(zip(lb, ub)),
@@ -406,7 +462,7 @@ def solve(samp, xyz_of_samp, _cost, method, cx_is_positive=False):
     elif method == "L-BFGS-B":
         ## 'L-BFGS-B' Is crazy fast but doesn't quite land at 0.0000 error
         x_guess0 = scipy.optimize.minimize(
-            lambda x: costx(x[params_anch:], x[0:params_anch]),
+            lambda x: costx(x[params_anch:-params_buildup], x[0:params_anch], x[-params_buildup], x[-params_buildup+1:]),
             x_guess0,
             method=method,
             bounds=list(zip(lb, ub)),
@@ -514,10 +570,10 @@ if __name__ == "__main__":
     # Does not affect optimization result. Only used for manual sanity check.
     anchors = np.array(
         [
-            [0.0, -1112.0, -115.0],
-            [970.0, 550.0, -115.0],
-            [-970.0, 550.0, -115.0],
-            [0.0, 0.0, 2865.0],
+            [0, -1620, -10],
+            [1800 * np.cos(np.pi / 5), 1800 * np.sin(np.pi / 5), -10],
+            [-1620 * np.cos(np.pi / 6), 1620 * np.sin(np.pi / 6), -10],
+            [0, 0, 2350],
         ]
     )
 
@@ -548,34 +604,50 @@ if __name__ == "__main__":
             ]
         )
 
-    samp = args["sample_data"]
-    if np.size(samp) != 0:
-        if np.size(samp) % 4 != 0:
-            print("Please specify A, B, C, and D diffs of sampling points.")
+    motor_pos_samp = args["sample_data"]
+    if np.size(motor_pos_samp) != 0:
+        if np.size(motor_pos_samp) % 4 != 0:
+            print("Please specify motor positions (angles) of sampling points.")
             print(
                 "You specified %d numbers after your -s/--sample_data option, which is not a multiple of 4 number of numbers."
             )
             sys.exit(1)
-        samp = samp.reshape((int(np.size(samp) / 4), 4))
+        motor_pos_samp = motor_pos_samp.reshape((int(np.size(motor_pos_samp) / 4), 4))
     else:
         # You might want to manually replace this with your collected data
-        samp = np.array(
+        motor_pos_samp = np.array(
             [
-                [400.53, 175.53, 166.10, -656.90],
-                [229.27, 511.14, -48.41, -554.31],
-                [-41.69, -62.87, 306.76, -225.31],
-                [272.97, 176.65, 381.13, -717.81],
-                [338.07, 633.70, 309.27, -911.22],
-                [504.47, 658.88, 48.60, -794.42],
-                [504.47, 658.88, 48.60, -794.42],
-                [103.50, 569.98, 633.68, -860.25],
-                [229.37, 7.32, 411.98, -575.81],
-                [428.73, -413.46, 250.38, -133.93],
-                [-506.97, 343.33, 327.68, -4.40],
+                #[    -0.0  ,     -0.0  ,     -0.0  ,     -0.0  ],
+                [ -1559.522,  35112.937,  11052.954,  -8872.595], # Simulated data w no fuzz
+                [ 15947.869,  44648.943,  25081.529, -20058.220],
+                [  6380.425,  19670.677, -16066.842,   4584.479],
+                [ 12033.385,  23821.841,  -5934.602, -15049.766],
+                [ 25842.555,  34790.088,  13138.384, -28972.382],
+                [ 26651.024,  14774.897, -26368.609,   8830.624],
+                [ 30623.769,  19265.113, -11595.884,  -8872.595],
+                [ 41211.318,  30931.858,   9866.130, -20058.220],
+                [-22455.217,  16219.845,  15070.811,   4584.479],
+                [ -9771.808,  20604.526,  19861.334, -15049.766],
+                [ 10871.057,  32057.528,  32119.331, -28972.382],
+                [ -9961.996,  31610.402,   5267.875,   8830.624],
+                [  6498.393,   5934.960,   6498.393, -22456.334],
+                [ 21632.763,  20205.165,  21632.763, -44870.348],
+                [ 22498.630,  -7698.495,  -4589.645,   4584.479],
+                #[ 26731.305,   -604.054,   2676.888, -15049.766],
+                #[ 37871.408,  15355.987,  18863.009, -28972.382],
+                #[ -9961.996,   6924.497,  31248.118,   8830.624],
+                #[ -1559.522,  12086.068,  34967.311,  -8872.595],
+                #[ 15947.869,  25035.973,  44996.327, -20058.220],
+                #[  6380.425, -14551.956,  20602.771,   4584.479],
+                #[ 12033.385,  -6018.578,  24965.465, -15049.766],
+                #[ 25842.555,  11621.846,  36374.371, -28972.382],
+                #[ 26651.024, -30082.168,  17780.735,   8830.624],
+                #[ 30623.769, -15504.967,  22351.992,  -8872.595],
+                [ 41211.318,   5888.695,  34181.575, -20058.220]
             ]
         )
 
-    u = np.shape(samp)[0]
+    u = np.shape(motor_pos_samp)[0]
     ux = np.shape(xyz_of_samp)[0]
     if ux > u:
         print("Error: You have more xyz positions than samples!")
@@ -583,23 +655,31 @@ if __name__ == "__main__":
         sys.exit(1)
 
     st1 = timeit.default_timer()
-    solution = solve(samp, xyz_of_samp, cost_sq, args["method"], args["cx_is_positive"])
+    solution = solve(motor_pos_samp, xyz_of_samp, cost_sq_for_pos_samp, args["method"], args["cx_is_positive"])
     st2 = timeit.default_timer()
     sol_anch = anchorsvec2matrix(solution[0:params_anch])
     sol_pos = np.zeros((u, 3))
+    sol_spool_buildup_factor = solution[-params_buildup]
+    sol_spool_r = solution[-params_buildup+1:]
 
     if np.size(xyz_of_samp) != 0:
         sol_pos = np.vstack(
-            (xyz_of_samp, np.reshape(solution[params_anch:], (u - ux, 3)))
+            (xyz_of_samp, np.reshape(solution[params_anch:-params_buildup], (u - ux, 3)))
         )
-        the_cost = cost_sq(anchorsvec2matrix(solution[0:params_anch]), sol_pos, samp)
     else:
-        sol_pos = np.reshape(solution[params_anch:], (u, 3))
-        the_cost = cost_sq(anchorsvec2matrix(solution[0:params_anch]), sol_pos, samp)
-    print("samples:          %d" % u)
-    print("input xyz coords: %d" % (3 * ux))
-    print("total cost:       %f" % the_cost)
-    print("cost per sample:  %f" % (the_cost / u))
+        sol_pos = np.reshape(solution[params_anch:-params_buildup], (u, 3))
+
+    the_cost = cost_sq_for_pos_samp(
+        anchorsvec2matrix(solution[0:params_anch]),
+        sol_pos,
+        motor_pos_samp,
+        sol_spool_buildup_factor,
+        np.array([sol_spool_r]),
+    )
+    print("number of samples: %d" % u)
+    print("input xyz coords:  %d" % (3 * ux))
+    print("total cost:        %f" % the_cost)
+    print("cost per sample:   %f" % (the_cost / u))
 
     if (u + 3 * ux) < params_anch:
         print("\nError: Lack of data detected.\n       Collect more samples.")
@@ -613,9 +693,41 @@ if __name__ == "__main__":
         )
 
     print_anch(sol_anch)
+    print("Spool buildup factor:", sol_spool_buildup_factor)
+    print("Spool radii:", sol_spool_r)
     if args["debug"]:
         print_anch_err(sol_anch, anchors)
         print("Method: %s" % args["method"])
         print("RUN TIME : {0}".format(st2 - st1))
-        print("positions: ")
-        print(sol_pos)
+        example_data_pos = np.array([[-1000., -1000.,  1000.],
+                                     [-1000., -1000.,  2000.],
+                                     [-1000.,     0.,     0.],
+                                     [-1000.,     0.,  1000.],
+                                     [-1000.,     0.,  2000.],
+                                     [-1000.,  1000.,     0.],
+                                     [-1000.,  1000.,  1000.],
+                                     [-1000.,  1000.,  2000.],
+                                     [    0., -1000.,     0.],
+                                     [    0., -1000.,  1000.],
+                                     [    0., -1000.,  2000.],
+                                     [-1000., -1000.,     0.],
+                                     [    0.,     0.,  1000.],
+                                     [    0.,     0.,  2000.],
+                                     [    0.,  1000.,     0.],
+                                     #[    0.,  1000.,  1000.],
+                                     #[    0.,  1000.,  2000.],
+                                     #[ 1000., -1000.,     0.],
+                                     #[ 1000., -1000.,  1000.],
+                                     #[ 1000., -1000.,  2000.],
+                                     #[ 1000.,     0.,     0.],
+                                     #[ 1000.,     0.,  1000.],
+                                     #[ 1000.,     0.,  2000.],
+                                     #[ 1000.,  1000.,     0.],
+                                     #[ 1000.,  1000.,  1000.],
+                                     [ 1000.,  1000.,  2000.]])
+        np.set_printoptions(precision=6)
+        np.set_printoptions(suppress=True) # No scientific notation
+        print("pos err: ")
+        print(sol_pos - example_data_pos)
+        print("spool_buildup_compensation err: %1.6f" % (sol_spool_buildup_factor - 0.008))
+        print("spool_r err:", sol_spool_r - np.array([65, 65, 65, 65]))
