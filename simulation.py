fc6cdc041b820b6b75537e6e2b758ae2d442cc77
tobbelobb <tobben@fastmail.fm>
2019-02-02 21:41:22 +0100

    Makes BuckShot return best result
    Also changes differentialEvolution strategy to Best1Exp
    And adds some more collected data

--- simulation.py
+++ simulation.py
@@ -169,7 +169,7 @@ def motor_pos_samples_with_spool_buildup_compensation(
     spool_r_in_origin_sq = spool_r_in_origin * spool_r_in_origin
 
     # Buildup per line times lines. Minus sign because more line in air means less line on spool
-    k2 = -mech_adv * number_of_lines_per_spool * spool_buildup_factor
+    k2 = -1.0 * mech_adv * number_of_lines_per_spool * spool_buildup_factor
 
     # we now want to use degrees instead of steps as unit of rotation
     # so setting 360 where steps per motor rotation is in firmware buildup compensation algorithms
@@ -178,7 +178,7 @@ def motor_pos_samples_with_spool_buildup_compensation(
     )
     k0 = 2.0 * degrees_per_unit_times_r / k2
 
-    line_lengths_origin = np.linalg.norm(anchors - np.array([[[0, 0, 0]]]), 2, 2)
+    line_lengths_origin = np.linalg.norm(anchors, 2, 1)
 
     relative_line_lengths = samples_relative_to_origin_no_fuzz(anchors, pos)
     motor_positions = k0 * (
@@ -197,7 +197,7 @@ def motor_pos_samples_to_line_length_with_buildup_compensation(
     number_of_lines_per_spool=np.array([1.0, 1.0, 1.0, 1.0]),  # HP4 default
 ):
     # Buildup per line times lines. Minus sign because more line in air means less line on spool
-    c1 = -mech_adv * number_of_lines_per_spool * spool_buildup_factor
+    c1 = -1.0 * mech_adv * number_of_lines_per_spool * spool_buildup_factor
 
     # we now want to use degrees instead of steps as unit of rotation
     # so setting 360 where steps per motor rotation is in firmware buildup compensation algorithms
@@ -404,36 +404,36 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
     # -20.0 <  z  < 3400.0
     # Define bounds
     lb = (
-        [
-            -l_long,  # A_ay > -4000.0
-            -300.0,  # A_az > -300.0
-            300.0,  # A_bx > 300
-            300.0,  # A_by > 300
-            -300.0,  # A_bz > -300.0
-            -l_long,  # A_cx > -4000
-            300.0,  # A_cy > 300
-            -300.0,  # A_cz > -300.0
-            1000.0,  # A_dz > 1000
-        ]
-        + [-l_short, -l_short, data_z_min] * (u - ux)
-        + [0.00005, 64.6, 64.6, 64.6, 64.6]
+       [
+           -l_long,  # A_ay > -4000.0
+           -300.0,  # A_az > -300.0
+           300.0,  # A_bx > 300
+           300.0,  # A_by > 300
+           -300.0,  # A_bz > -300.0
+           -l_long,  # A_cx > -4000
+           300.0,  # A_cy > 300
+           -300.0,  # A_cz > -300.0
+           1000.0,  # A_dz > 1000
+       ]
+       + [-l_short, -l_short, data_z_min] * (u - ux)
+       + [0.00005, 64.6, 64.6, 64.6, 64.6]
     )
     ub = (
-        [
-            500.0,  # A_ay < 500
-            200.0,  # A_az < 200
-            l_long,  # A_bx < 4000
-            l_long,  # A_by < 4000
-            200.0,  # A_bz < 200
-            -300.0,  # A_cx < -300
-            l_long,  # A_cy < 4000.0
-            200.0,  # A_cz < 200
-            l_long,  # A_dz < 4000.0
-        ]
-        + [l_short, l_short, 2 * l_short] * (u - ux)
-        + [0.01, 67, 67, 67, 67]
+       [
+           500.0,  # A_ay < 500
+           200.0,  # A_az < 200
+           l_long,  # A_bx < 4000
+           l_long,  # A_by < 4000
+           200.0,  # A_bz < 200
+           -300.0,  # A_cx < -300
+           l_long,  # A_cy < 4000.0
+           200.0,  # A_cz < 200
+           l_long,  # A_dz < 4000.0
+       ]
+       + [l_short, l_short, 2 * l_short] * (u - ux)
+       + [0.01, 67, 67, 67, 67]
     )
-    # lb = (
+    #lb = (
     #    [
     #        -2000.0,  # A_ay > -2000.0
     #        -200.0,  # A_az > -200.0
@@ -447,8 +447,8 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
     #    ]
     #    + [-l_short, -l_short, data_z_min] * (u - ux)
     #    + [0.00005, 64.6, 64.6, 64.6, 64.6]
-    # )
-    # ub = (
+    #)
+    #ub = (
     #    [
     #        -1600.0,  # A_ay < -1600
     #        0.0,  # A_az < 0
@@ -462,7 +462,7 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
     #    ]
     #    + [l_short, l_short, 2 * l_short] * (u - ux)
     #    + [0.01, 67, 67, 67, 67]
-    # )
+    #)
     # lb = (
     #    [
     #        -999999.0,  # A_ay > -4000.0
@@ -554,12 +554,14 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
         from mystic.solvers import DifferentialEvolutionSolver2
         from mystic.monitors import VerboseMonitor
         from mystic.termination import VTR, ChangeOverGeneration, And, Or
+        from mystic.strategy import Best1Exp
 
         stop = Or(VTR(1e-12), ChangeOverGeneration(0.0000001, 5000))
         ndim = number_of_params_pos + params_anch + params_buildup
         npop = 30 * ndim
         stepmon = VerboseMonitor(100)
         solver = DifferentialEvolutionSolver2(ndim, npop)
+
         solver.SetRandomInitialPoints(lb, ub)
         solver.SetStrictRanges(lb, ub)
         solver.SetConstraints(constraints)
@@ -575,6 +577,7 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
                 u,
             ),
             termination=stop,
+            strategy=Best1Exp,
         )
 
         # use monitor to retrieve results information
@@ -639,18 +642,7 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
         )  # ,
         # constraints=constraints)
         searcher._summarize()
-
-        xy, param, cost = searcher.Trajectories()
-        param = np.array(param)
-        cost = np.array(cost)
-
-        best_cost = 99999999.9
-        best_i = 0
-        for i in range(len(cost)):
-            if cost[i] < best_cost:
-                best_cost = cost[i]
-                best_i = i
-        return param[best_i]
+        return np.array(min(searcher.Minima(), key=searcher.Minima().get))
 
     elif method == "PowellDirectionalSolver":
         from mystic.solvers import PowellDirectionalSolver
@@ -784,7 +776,7 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
             x_guess,
             method="SLSQP",
             bounds=list(zip(lb, ub)),
-            #constraints=[{'type': 'eq', 'fun': constraints}], # This doesn't seem to work?
+            # constraints=[{'type': 'eq', 'fun': constraints}], # This doesn't seem to work?
             options={"disp": True, "ftol": 1e-20, "maxiter": 150000},
         ).x
         return x_guess
@@ -811,7 +803,7 @@ def solve(motor_pos_samp, xyz_of_samp, method, cx_is_positive=False):
         sys.exit(1)
 
 
-def print_anch(anch):
+def print_copypasteable(anch, spool_buildup_factor, spool_r):
     print("\n#define ANCHOR_A_Y %5d" % round(anch[A, Y]))
     print("#define ANCHOR_A_Z %5d" % round(anch[A, Z]))
     print("#define ANCHOR_B_X %5d" % round(anch[B, X]))
@@ -833,6 +825,25 @@ def print_anch(anch):
             anch[C, Y],
             anch[C, Z],
             anch[D, Z],
+        )
+    )
+    print(
+        "\nM669 A0.0:%.2f:%.2f B%.2f:%.2f:%.2f C%.2f:%.2f:%.2f D%.2f Q%.6f R%.3f:%.3f:%.3f:%.3f"
+        % (
+            anch[A, Y],
+            anch[A, Z],
+            anch[B, X],
+            anch[B, Y],
+            anch[B, Z],
+            anch[C, X],
+            anch[C, Y],
+            anch[C, Z],
+            anch[D, Z],
+            spool_buildup_factor,
+            spool_r[A],
+            spool_r[B],
+            spool_r[C],
+            spool_r[D],
         )
     )
 
@@ -873,7 +884,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m",
         "--method",
-        help='Available methods are L-BFGS-B (0), PowellDirectionalSolver (1), SLSQP (2), differentialEvolutionSolver (3), BuckShot (4), and all (5). Try 0 first, then 1, and so on.',
+        help="Available methods are L-BFGS-B (0), PowellDirectionalSolver (1), SLSQP (2), differentialEvolutionSolver (3), BuckShot (4), and all (5). Try 0 first, then 1, and so on.",
         default="PowellDirectionalSolver",
     )
     parser.add_argument(
@@ -959,59 +970,80 @@ if __name__ == "__main__":
         motor_pos_samp = motor_pos_samp.reshape((int(np.size(motor_pos_samp) / 4), 4))
     else:
         # You might want to manually replace this with your collected data
-        # motor_pos_samp = np.array(
-        #        [
-        #            [  2185.65,-   1565.88,    901.40,- 3497.24, ], # First collected data. B and D had wrong signs
-        #            [ 13289.12,-  16251.47,   9180.44,- -35.29, ],
-        #            [ 19102.38,-  19737.29,  10106.74,- -34.05, ],
-        #            [ 15736.09,-  12568.62,   7477.67,- 6882.47, ],
-        #            [ 11833.54,-   3475.36,   7092.05,- 15260.72, ],
-        #            [ 10453.83,-  -8991.93,   7252.55,- 24340.97, ],
-        #            [ 12342.05,-   -286.95,   3259.66,- 16596.90, ],
-        #            [ 12341.57,-   4064.08,   -840.31,- 8899.52, ],
-        #            [ 14965.81,-   4064.08,  -6865.62,- 2128.97, ],
-        #            [ 16145.44,-  -5181.89, -15089.08,- 2098.95, ],
-        #            [ 11140.04,-  -5179.96,  -9597.86,- 7383.00, ],
-        #            [  5789.15,- -11239.36,  -4038.70,- 13199.07, ],
-        #            [ -7015.04,- -14092.20,   5979.81,- 10498.90, ],
-        #            [-10983.31,- -10043.28,   6839.74,- 2878.31, ],
-        #            [ -7130.69,- -10049.42,   6989.61,- 9051.27, ],
-        #            [ -3162.74,-  -5991.19,   2751.78,- 7802.40, ],
-        #            [  2492.55,-    -45.91,    361.06,- 6290.42, ],
-        #        ]
-        #        )
-        ### Simulated data w no fuzz ###
-        motor_pos_samp = np.array(
-            [
-                # [    -0.0  ,     -0.0  ,     -0.0  ,     -0.0  ],
-                [-1559.522, 35112.937, 11052.954, -8872.595],
-                [15947.869, 44648.943, 25081.529, -20058.220],
-                [6380.425, 19670.677, -16066.842, 4584.479],
-                [12033.385, 23821.841, -5934.602, -15049.766],
-                [25842.555, 34790.088, 13138.384, -28972.382],
-                [26651.024, 14774.897, -26368.609, 8830.624],
-                [30623.769, 19265.113, -11595.884, -8872.595],
-                [41211.318, 30931.858, 9866.130, -20058.220],
-                [-22455.217, 16219.845, 15070.811, 4584.479],
-                [-9771.808, 20604.526, 19861.334, -15049.766],
-                [10871.057, 32057.528, 32119.331, -28972.382],
-                [-9961.996, 31610.402, 5267.875, 8830.624],
-                [6498.393, 5934.960, 6498.393, -22456.334],
-                [21632.763, 20205.165, 21632.763, -44870.348],
-                [22498.630, -7698.495, -4589.645, 4584.479],
-                [26731.305, -604.054, 2676.888, -15049.766],
-                [37871.408, 15355.987, 18863.009, -28972.382],
-                [-9961.996, 6924.497, 31248.118, 8830.624],
-                [-1559.522, 12086.068, 34967.311, -8872.595],
-                [15947.869, 25035.973, 44996.327, -20058.220],
-                [6380.425, -14551.956, 20602.771, 4584.479],
-                [12033.385, -6018.578, 24965.465, -15049.766],
-                [25842.555, 11621.846, 36374.371, -28972.382],
-                [26651.024, -30082.168, 17780.735, 8830.624],
-                [30623.769, -15504.967, 22351.992, -8872.595],
-                [41211.318, 5888.695, 34181.575, -20058.220],
-            ]
-        )
+    #    motor_pos_samp = np.array(
+    #        [
+    #            [2185.65, -1565.88, 901.40, -3497.24],
+    #            [13289.12, -16251.47, 9180.44, 35.29],
+    #            [19102.38, -19737.29, 10106.74, 34.05],
+    #            [15736.09, -12568.62, 7477.67, -6882.47],
+    #            [11833.54, -3475.36, 7092.05, -15260.72],
+    #            [10453.83, 8991.93, 7252.55, -24340.97],
+    #            [12342.05, 286.95, 3259.66, -16596.90],
+    #            [12341.57, -4064.08, -840.31, -8899.52],
+    #            [14965.81, -4064.08, -6865.62, -2128.97],
+    #            [16145.44, 5181.89, -15089.08, -2098.95],
+    #            [11140.04, 5179.96, -9597.86, -7383.00],
+    #            [5789.15, 11239.36, -4038.70, -13199.07],
+    #            [-7015.04, 14092.20, 5979.81, -10498.90],
+    #            [-10983.31, 10043.28, 6839.74, -2878.31],
+    #            [-7130.69, 10049.42, 6989.61, -9051.27],
+    #            [-3162.74, 5991.19, 2751.78, -7802.40],
+    #            [2492.55, 45.91, 361.06, -6290.42],
+    #            [19829.39, -5890.33, 7533.87, -15096.64],
+    #            [14754.43, -4818.78, -3363.34, -6367.55],
+    #            [14912.98, -4818.78, -3387.19, -6370.97],
+    #            [10131.60, -8274.28, 315.28, -2108.26],
+    #            [7768.62, -2569.03, -4573.51, -2099.56],
+    #            [3141.41, 4041.86, -6660.20, -2098.82],
+    #            [-2441.61, 10314.15, -4563.80, -2843.30],
+    #            [8277.75, 8616.45, -8343.78, -9353.54],
+    #            [16481.21, 3228.76, -2106.80, -15410.39],
+    #            [12644.83, -1824.00, 4526.38, -15778.54],
+    #            [8674.19, 1957.49, 2777.83, -15906.66],
+    #            [6238.78, 5579.11, 25.78, -14755.41],
+    #            [-5335.10, 8043.92, 4345.76, -8144.45],
+    #            [-3890.16, 3188.10, 1555.36, -149.26],
+    #            [-1394.91, 3188.10, 896.54, -5248.52],
+    #            [2026.63, 2691.43, -3303.89, -4314.29],
+    #            [3962.08, 504.26, -2633.29, -5188.25],
+    #            [5130.26, -1638.34, -3161.16, -2275.37],
+    #            [3713.22, -1638.34, -361.39, -4574.90],
+    #            [2859.11, -2159.69, 1389.60, -4575.80],
+    #            [1492.21, -664.47, -102.30, -2519.54],
+    #        ]
+    #    )
+    ### Simulated data w no fuzz ###
+    motor_pos_samp = np.array(
+       [
+           # [    -0.0  ,     -0.0  ,     -0.0  ,     -0.0  ],
+           [-1559.522, 35112.937, 11052.954, -8872.595],
+           [15947.869, 44648.943, 25081.529, -20058.220],
+           [6380.425, 19670.677, -16066.842, 4584.479],
+           [12033.385, 23821.841, -5934.602, -15049.766],
+           [25842.555, 34790.088, 13138.384, -28972.382],
+           [26651.024, 14774.897, -26368.609, 8830.624],
+           [30623.769, 19265.113, -11595.884, -8872.595],
+           [41211.318, 30931.858, 9866.130, -20058.220],
+           [-22455.217, 16219.845, 15070.811, 4584.479],
+           [-9771.808, 20604.526, 19861.334, -15049.766],
+           [10871.057, 32057.528, 32119.331, -28972.382],
+           [-9961.996, 31610.402, 5267.875, 8830.624],
+           [6498.393, 5934.960, 6498.393, -22456.334],
+           [21632.763, 20205.165, 21632.763, -44870.348],
+           [22498.630, -7698.495, -4589.645, 4584.479],
+           [26731.305, -604.054, 2676.888, -15049.766],
+           [37871.408, 15355.987, 18863.009, -28972.382],
+           [-9961.996, 6924.497, 31248.118, 8830.624],
+           [-1559.522, 12086.068, 34967.311, -8872.595],
+           [15947.869, 25035.973, 44996.327, -20058.220],
+           [6380.425, -14551.956, 20602.771, 4584.479],
+           [12033.385, -6018.578, 24965.465, -15049.766],
+           [25842.555, 11621.846, 36374.371, -28972.382],
+           [26651.024, -30082.168, 17780.735, 8830.624],
+           [30623.769, -15504.967, 22351.992, -8872.595],
+           [41211.318, 5888.695, 34181.575, -20058.220],
+       ]
+    )
 
     u = np.shape(motor_pos_samp)[0]
     ux = np.shape(xyz_of_samp)[0]
@@ -1116,50 +1148,51 @@ if __name__ == "__main__":
             "\nWarning: Data set might be too small.\n         The below values are unreliable unless input data is extremely accurate."
         )
 
-    print_anch(the_cand.anch)
+    print_copypasteable(the_cand.anch, the_cand.spool_buildup_factor, the_cand.spool_r)
     print("Spool buildup factor:", the_cand.spool_buildup_factor)
     print("Spool radii:", the_cand.spool_r)
     if args["debug"]:
         print_anch_err(the_cand.anch, anchors)
         print("Method: %s" % args["method"])
         print("RUN TIME : {0}".format(st2 - st1))
-        example_data_pos = np.array(
-            [
-                [-1000.0, -1000.0, 1000.0],
-                [-1000.0, -1000.0, 2000.0],
-                [-1000.0, 0.0, 0.0],
-                [-1000.0, 0.0, 1000.0],
-                [-1000.0, 0.0, 2000.0],
-                [-1000.0, 1000.0, 0.0],
-                [-1000.0, 1000.0, 1000.0],
-                [-1000.0, 1000.0, 2000.0],
-                [0.0, -1000.0, 0.0],
-                [0.0, -1000.0, 1000.0],
-                [0.0, -1000.0, 2000.0],
-                [-1000.0, -1000.0, 0.0],
-                [0.0, 0.0, 1000.0],
-                [0.0, 0.0, 2000.0],
-                [0.0, 1000.0, 0.0],
-                [0.0, 1000.0, 1000.0],
-                [0.0, 1000.0, 2000.0],
-                [1000.0, -1000.0, 0.0],
-                [1000.0, -1000.0, 1000.0],
-                [1000.0, -1000.0, 2000.0],
-                [1000.0, 0.0, 0.0],
-                [1000.0, 0.0, 1000.0],
-                [1000.0, 0.0, 2000.0],
-                [1000.0, 1000.0, 0.0],
-                [1000.0, 1000.0, 1000.0],
-                [1000.0, 1000.0, 2000.0],
-            ]
-        )
         np.set_printoptions(precision=6)
         np.set_printoptions(suppress=True)  # No scientific notation
-        print("pos err: ")
-        print(the_cand.pos - example_data_pos)
+        print("Data collected at positions: ")
         print(the_cand.pos)
-        print(
-            "spool_buildup_compensation err: %1.6f"
-            % (the_cand.spool_buildup_factor - 0.008)
-        )
-        print("spool_r err:", the_cand.spool_r - np.array([65, 65, 65, 65]))
+        # example_data_pos = np.array(
+        #    [
+        #        [-1000.0, -1000.0, 1000.0],
+        #        [-1000.0, -1000.0, 2000.0],
+        #        [-1000.0, 0.0, 0.0],
+        #        [-1000.0, 0.0, 1000.0],
+        #        [-1000.0, 0.0, 2000.0],
+        #        [-1000.0, 1000.0, 0.0],
+        #        [-1000.0, 1000.0, 1000.0],
+        #        [-1000.0, 1000.0, 2000.0],
+        #        [0.0, -1000.0, 0.0],
+        #        [0.0, -1000.0, 1000.0],
+        #        [0.0, -1000.0, 2000.0],
+        #        [-1000.0, -1000.0, 0.0],
+        #        [0.0, 0.0, 1000.0],
+        #        [0.0, 0.0, 2000.0],
+        #        [0.0, 1000.0, 0.0],
+        #        [0.0, 1000.0, 1000.0],
+        #        [0.0, 1000.0, 2000.0],
+        #        [1000.0, -1000.0, 0.0],
+        #        [1000.0, -1000.0, 1000.0],
+        #        [1000.0, -1000.0, 2000.0],
+        #        [1000.0, 0.0, 0.0],
+        #        [1000.0, 0.0, 1000.0],
+        #        [1000.0, 0.0, 2000.0],
+        #        [1000.0, 1000.0, 0.0],
+        #        [1000.0, 1000.0, 1000.0],
+        #        [1000.0, 1000.0, 2000.0],
+        #    ]
+        # )
+        # print("pos err: ")
+        # print(the_cand.pos - example_data_pos)
+        #print(
+        #    "spool_buildup_compensation err: %1.6f"
+        #    % (the_cand.spool_buildup_factor - 0.008)
+        #)
+        #print("spool_r err:", the_cand.spool_r - np.array([65, 65, 65, 65]))
