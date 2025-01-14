b6f5985db93732e3c3534059969bd8f252493b45
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-01-14 13:50:40 +0100

    Reduced buildup compensation to reflect change from 1.1 mm line (Garda) to 0.87 mm line (DC Pro 161)

--- simulation.py
+++ simulation.py
@@ -16,7 +16,7 @@ from hangprinter_forward_transform import forward_transform
 from flex_distance import *
 
 # Config values should be based on your physical machine
-constant_spool_buildup_factor = 0.006875 * 10  # Qualified first guess for 1.1 mm line
+constant_spool_buildup_factor = 0.0043 * 10  # Qualified first guess for 0.87 mm line
 spool_r_in_origin_first_guess = np.array([75.0, 75.0, 75.0, 75.0])
 spool_gear_teeth = 255
 motor_gear_teeth = 20
